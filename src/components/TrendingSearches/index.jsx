3f3fe4c08d0f5421ec1a560e293835ee6647107c
IssaiMV <mendozaissai@gmail.com>
2021-05-05 16:11:11 -0500

    FEAT: Lazy Load
    Si no funciona, deberás agregar un min-height

--- src/components/TrendingSearches/index.jsx
+++ src/components/TrendingSearches/index.jsx
@@ -2,11 +2,32 @@ import { useEffect, useState } from "react";
 import getTrendingTerms from "services/getTrendingTermsService";
 import Category from "components/Category/";
 
-export default function TrendingSearches() {
+function TrendingSearches() {
   const [trends, setTrends] = useState([]);
+
   useEffect(() => {
     getTrendingTerms().then(setTrends);
   }, []);
 
   return <Category name="Tendencias" options={trends} />;
+}
+
+export default function LazyTrending() {
+  const [show, setShow] = useState(false);
+
+  useEffect(() => {
+    const onChange = (entries) => {
+      const el = entries[0];
+      console.log(el.isIntersecting);
+      if (el.isIntersecting) {
+        setShow(true);
+      }
+    };
+    const observer = new IntersectionObserver(onChange, {
+      rootMargin: "100px",
+    });
+    observer.observe(document.getElementById("LazyTrending"));
+  }, []);
+
+  return <div id="LazyTrending">{show ? <TrendingSearches /> : null}</div>;
 }
