2bbd9426e476ccbffbfa9a912ff4006af8482e63
IssaiMV <mendozaissai@gmail.com>
2021-05-18 23:20:42 -0500

    FEAT: Se agrego React.lazy
    Se usa para cargar los elementos cuando los necesites

--- src/components/TrendingSearches/index.jsx
+++ src/components/TrendingSearches/index.jsx
@@ -1,33 +1,17 @@
-import { useEffect, useState } from "react";
-import getTrendingTerms from "services/getTrendingTermsService";
-import Category from "components/Category/";
+import React, { Suspense } from "react";
+import useNearScreen from "hooks/useNearScreen";
+import Loading from "components/Loading";
 
-function TrendingSearches() {
-  const [trends, setTrends] = useState([]);
-
-  useEffect(() => {
-    getTrendingTerms().then(setTrends);
-  }, []);
-
-  return <Category name="Tendencias" options={trends} />;
-}
+const TrendingSearches = React.lazy(() => import("./TrendingSearches"));
 
 export default function LazyTrending() {
-  const [show, setShow] = useState(false);
+  const { isNearScreen, fromRef } = useNearScreen({ distance: "200px" });
 
-  useEffect(() => {
-    const onChange = (entries) => {
-      const el = entries[0];
-      console.log(el.isIntersecting);
-      if (el.isIntersecting) {
-        setShow(true);
-      }
-    };
-    const observer = new IntersectionObserver(onChange, {
-      rootMargin: "100px",
-    });
-    observer.observe(document.getElementById("LazyTrending"));
-  }, []);
-
-  return <div id="LazyTrending">{show ? <TrendingSearches /> : null}</div>;
+  return (
+    <div ref={fromRef}>
+      <Suspense fallback={<Loading />}>
+        {isNearScreen ? <TrendingSearches /> : null}
+      </Suspense>
+    </div>
+  );
 }
